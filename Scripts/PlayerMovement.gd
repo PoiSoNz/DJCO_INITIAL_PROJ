f3c325f4f6ad293b3cadfee11e7d3c33c9e74dca
Nelson Almeida <up201505394@fe.up.pt>
2019-02-25 02:28:55 +0000

    add player deacceleration when no movement key is being pressed

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -6,7 +6,8 @@ var jumpCount = 2
 const floorNormal = Vector2(0, -1)
 const gravity = Vector2(0, 9.8)
 const runAcceleration = Vector2(30, 0)
-const jumpAcceleration = Vector2(0, -500)
+const runDeacceleration = Vector2(10, 0)
+const jumpAcceleration = Vector2(0, -700)
 const maxVelocity = 500
 
 # Called when the node enters the scene tree for the first time.
@@ -36,12 +37,23 @@ func reset_gravity():
 	velocity.y = 0
 
 func player_movement():
-	if Input.is_key_pressed(KEY_RIGHT):
+	# Run right
+	if Input.is_key_pressed(KEY_RIGHT) && !Input.is_key_pressed(KEY_LEFT) && velocity.x >= 0:
 		if velocity.x < maxVelocity:
 			velocity += runAcceleration
-	if Input.is_key_pressed(KEY_LEFT):
+	# Run left
+	elif Input.is_key_pressed(KEY_LEFT) && !Input.is_key_pressed(KEY_RIGHT) && velocity.x <= 0:
 		if velocity.x > -maxVelocity:
 			velocity -= runAcceleration
+	# Run deacceleration (no movement key is being pressed or both are being pressed)
+	else:
+		if velocity.x - runDeacceleration.x >= 0:
+			velocity -= runDeacceleration
+		elif velocity.x + runDeacceleration.x <= 0:
+			velocity += runDeacceleration
+		else:
+			velocity.x = 0
+			
 	if Input.is_action_just_pressed("player_jump") && jumpCount > 0:
 		jumpCount -= 1
 		reset_gravity()
