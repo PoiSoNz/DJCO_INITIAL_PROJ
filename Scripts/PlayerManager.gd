96e59cbfa3780de8261f43e8ab8eb7fa48c1ce1d
Nelson Almeida <up201505394@fe.up.pt>
2019-02-28 20:52:25 +0000

    buy items shit

--- Scripts/PlayerManager.gd
+++ Scripts/PlayerManager.gd
@@ -2,6 +2,15 @@ extends Node
 
 var ECTS = 0
 
+const croissant_price = 40
+const water_price = 50
+const coffee_price = 70
+const special_merend_price = 150
+
+const croissant_hp_bonus = 40
+const coffee_speed_bonus = 40 #SPEED
+const special_merend_bonus = 40 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -13,10 +22,10 @@ func _ready():
 func buy_attempt(item):
 	match item:
 		"croissant":
-			print("comprou croissant")
+			if ECTS >= croissant_price:
 		"water":
-			print("comprou water")
+			if ECTS >= water_price:
 		"coffee":
-			print("comprou coffee")
+			if ECTS >= coffee_price:
 		"special_merend":
-			print("comprou special_merend")
+			if ECTS >= special_merend_price:
