31cb7fb80a49ad3e296a52756f447545c85f5680
Nelson Almeida <up201505394@fe.up.pt>
2019-03-08 01:59:26 +0000

    refactor duplicated code

--- Scripts/PlayerHealth.gd
+++ Scripts/PlayerHealth.gd
@@ -15,49 +15,39 @@ signal bled(oldHealth, newHealth)
 func _process(delta):
 	frameDelta = delta;
 	
-	var previousHealth = health
 	if bleed:
 		health_bleed(2)
-	var newHealth = health
-	
-	#print("previous: ", 
-	verify_hp_threshold(previousHealth, newHealth)
 
 func health_increment(gain):
-	var oldHP = health
-	
-	var newHP = health + gain
-	if newHP > 100:
-		newHP = 100
-	health = newHP
-	
-	verify_hp_threshold(oldHP, newHP)
+	var oldHP = change_health(gain)
 	
 	bleed = false
 	emit_signal("health_changed", oldHP, health)
 
 func reduce_health(damage):
-	var oldHP = health
-	
-	var newHP = health - damage
-	if newHP < 0 :
-		newHP = 0
-	health = newHP
-	
-	verify_hp_threshold(oldHP, newHP)
+	var oldHP = change_health(-damage)
 	
 	bleed = false
 	emit_signal("health_changed", oldHP, health)
 
 func health_bleed(value):
-	var oldHP = health
-	
-	var newHP = health - apply_delta(value)
-	if newHP < 0 :
-		newHP = 0
-	health = newHP
+	var oldHP = change_health(-apply_delta(value))
 	
 	emit_signal("bled", oldHP, health)
+
+func change_health(amount):
+	var oldHP = health
+	
+	var newHP = health + amount
+	if newHP < 0 :
+		newHP = 0
+	elif newHP > 100:
+		newHP = 100
+	health = newHP
+	
+	verify_hp_threshold(oldHP, newHP)
+	
+	return oldHP
 
 func verify_hp_threshold(previousHealth, newHealth):
 	if previousHealth > 30 && newHealth <= 30:
