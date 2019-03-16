a3a36070c97d3e41321d86ca2223f1b710d50542
JoseLuisOliveiraCunha <zeluiscunha@outlook.com>
2019-03-16 21:25:53 +0000

    fixed bench

--- Scripts/Bench.gd
+++ Scripts/Bench.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 const damage = 10
 
@@ -7,9 +7,14 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var collision_info = move_and_collide(Vector2(0, 0))
-	if collision_info:
-		var collider = collision_info.collider.get_parent()
-		if collider.name == "Player":
-			collider.inflict_damage(damage)
+#func _process(delta):
+#	var collision_info = move_and_collide(Vector2(0, 0))
+#	if collision_info:
+#		var collider = collision_info.collider.get_parent()
+#		if collider.name == "Player":
+#			collider.inflict_damage(damage)
+
+func _on_Area2D_body_entered(body):
+	if body.get_parent().name == "Player":
+		body.get_parent().inflict_damage(damage)
+
