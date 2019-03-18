995799507d54011331f4f27f90e2855f9765a1de
Nelson Almeida <up201505394@fe.up.pt>
2019-03-18 23:50:02 +0000

    level 4 - hard jump

---
+++ Scripts/Test4Manager.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	$GoldCoin.set_type(0)
+	$GoldCoin2.set_type(0)
+	$GoldCoin3.set_type(0)
+	$GoldCoin4.set_type(0)
+	$BronzeCoin.set_type(2)
+	$BronzeCoin2.set_type(2)
+	$BronzeCoin3.set_type(2)
+	$BronzeCoin4.set_type(2)
+	$VendingMachine.set_tier(3)
+	$VendingMachine2.set_tier(2)
