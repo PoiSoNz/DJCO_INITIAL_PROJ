e3f396c8a13bc5a4e3205635d1b286930aa2aa1e
Nelson Almeida <up201505394@fe.up.pt>
2019-03-20 19:49:15 +0000

    api calls tested, added animated label when on new highscore (not finished)

---
+++ Scripts/ShowHighcores.gd
@@ -0,0 +1,26 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("GameJoltAPI").fetch_scores() # Fetches 10 highscores by default
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_NewGameButton_pressed():
+	get_tree().change_scene("res://Scenes/MasterLevel.tscn")
+
+
+func _on_MainMenuButton_pressed():
+	#get_tree().change_scene("res://Scenes/MainMenu.tscn")
+	pass
+
+
+func _on_GameJoltAPI_api_scores_fetched(data):
+	pass # Replace with function body.
