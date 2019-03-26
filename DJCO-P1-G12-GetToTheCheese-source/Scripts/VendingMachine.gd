086d3a3cd80f5cf9a7dfdcaed53d9fa8485502c0
JoseLuisOliveiraCunha <zeluiscunha@outlook.com>
2019-03-26 17:33:39 +0000

    added price and key info to power-ups

--- DJCO-P1-G12-GetToTheCheese-source/Scripts/VendingMachine.gd
+++ DJCO-P1-G12-GetToTheCheese-source/Scripts/VendingMachine.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var grayscale = preload("res://Materials/SpriteGrayScale.tres")
 
+const font_grayscale = Color("6b6b6b")
+
 var playerInRange = false
 var playerNode
 var tier = 2
@@ -26,10 +28,22 @@ func set_tier(machineFloor):
 	tier = machineFloor
 	if tier == 2:
 		$Control/Coffee/Sprite.material = grayscale
+		$Control/Coffee/SpriteCoinIcon.material = grayscale
+		$Control/Coffee/LabelE.set("custom_colors/font_color", font_grayscale)
+		$Control/Coffee/LabelPrice.set("custom_colors/font_color", font_grayscale)
 		$Control/SpecialMerend/Sprite.material = grayscale
+		$Control/SpecialMerend/SpriteCoinIcon.material = grayscale
+		$Control/SpecialMerend/LabelR.set("custom_colors/font_color", font_grayscale)
+		$Control/SpecialMerend/LabelPrice.set("custom_colors/font_color", font_grayscale)
 	else:
 		$Control/Croissant/Sprite.material = grayscale
+		$Control/Croissant/SpriteCoinIcon.material = grayscale
+		$Control/Croissant/LabelQ.set("custom_colors/font_color", font_grayscale)
+		$Control/Croissant/LabelPrice.set("custom_colors/font_color", font_grayscale)
 		$Control/Water/Sprite.material = grayscale
+		$Control/Water/SpriteCoinIcon.material = grayscale
+		$Control/Water/LabelW.set("custom_colors/font_color", font_grayscale)
+		$Control/Water/LabelPrice.set("custom_colors/font_color", font_grayscale)
 
 func _on_Area2D_body_entered(body):
 	if body.get_parent().name == "Player":
